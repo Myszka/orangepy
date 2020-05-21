0e4a3e8336185ab161d693b0c9c94aa3cc14816d
MiCh <mich@ratsmilk.com>
2020-05-21 19:12:26 +0200

    remove of old htu functions added sd_notify and watchdog support

--- scripts/get_pms7003.py
+++ scripts/get_pms7003.py
@@ -9,6 +9,8 @@ import io, fcntl
 import subprocess
 import socket
 from datetime import datetime,timedelta
+import sys
+import sd_notify
 import os
 
 batcmd="cat /sys/class/leds/orangepi\:red\:status/brightness"
@@ -18,111 +20,17 @@ filenm='pms7003'
 hostname = socket.gethostname()
 IDstacji = 30100+int(hostname[-2:])
 
+notify = sd_notify.Notifier()
+
+if notify.enabled():
+	notify.status("Initialising PMS7003 ...")
+
 if not os.path.exists(datadir):
     os.makedirs(datadir)
 
-HTU21D_ADDR = 0x40
-CMD_READ_TEMP_HOLD = b"\xE3"
-CMD_READ_HUM_HOLD = b"\xE5"
-CMD_READ_TEMP_NOHOLD = b"\xF3"
-CMD_READ_HUM_NOHOLD = b"\xF5"
-CMD_WRITE_USER_REG = b"\xE6"
-CMD_READ_USER_REG = b"\xE7"
-CMD_SOFT_RESET = b"\xFE"
-I2C_SLAVE=0x0703
-
-
-
-class i2c(object):
-   def __init__(self, device, bus):
-      self.fr = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
-      self.fw = io.open("/dev/i2c-"+str(bus), "wb", buffering=0)
-      # set device address
-      fcntl.ioctl(self.fr, I2C_SLAVE, device)
-      fcntl.ioctl(self.fw, I2C_SLAVE, device)
-   def write(self, bytes):
-      self.fw.write(bytes)
-   def read(self, bytes):
-      return self.fr.read(bytes)
-   def close(self):
-      self.fw.close()
-      self.fr.close()
-
-
-class HTU21D(object):
-    def __init__(self):
-        self.dev = i2c(HTU21D_ADDR, 1)  # HTU21D 0x40, bus 1
-        self.dev.write(CMD_SOFT_RESET)  # Soft reset
-        time.sleep(.1)
-
-    def ctemp(self, sensor_temp):
-        t_sensor_temp = sensor_temp / 65536.0
-        return -46.85 + (175.72 * t_sensor_temp)
-
-    def chumid(self, sensor_humid):
-        t_sensor_humid = sensor_humid / 65536.0
-        return -6.0 + (125.0 * t_sensor_humid)
-
-    def temp_coefficient(self, rh_actual, temp_actual, coefficient=-0.15):
-        return rh_actual + (25 - temp_actual) * coefficient
-
-    def crc8check(self, value):
-        # Ported from Sparkfun Arduino HTU21D Library:
-        # https://github.com/sparkfun/HTU21D_Breakout
-        remainder = ((value[0] << 8) + value[1]) << 8
-        remainder |= value[2]
-
-        # POLYNOMIAL = 0x0131 = x^8 + x^5 + x^4 + 1 divisor =
-        # 0x988000 is the 0x0131 polynomial shifted to farthest
-        # left of three bytes
-        divisor = 0x988000
-
-        for i in range(0, 16):
-            if(remainder & 1 << (23 - i)):
-                remainder ^= divisor
-            divisor = divisor >> 1
-
-        if remainder == 0:
-            return True
-        else:
-            return False
-
-    def read_temperature(self):
-        self.dev.write(CMD_READ_TEMP_NOHOLD)  # Measure temp
-        time.sleep(.1)
-        data = self.dev.read(3)
-        buf = array.array('B', data)
-        if self.crc8check(buf):
-            temp = (buf[0] << 8 | buf[1]) & 0xFFFC
-            return self.ctemp(temp)
-        else:
-            return -255
-
-    def read_humidity(self):
-        temp_actual = self.read_temperature()  # For temperature coefficient compensation
-        self.dev.write(CMD_READ_HUM_NOHOLD)  # Measure humidity
-        time.sleep(.1)
-        data = self.dev.read(3)
-        buf = array.array('B', data)
-
-        if self.crc8check(buf):
-            humid = (buf[0] << 8 | buf[1]) & 0xFFFC
-            rh_actual = self.chumid(humid)
-
-            rh_final = self.temp_coefficient(rh_actual, temp_actual)
-
-            rh_final = 100.0 if rh_final > 100 else rh_final  # Clamp > 100
-            rh_final = 0.0 if rh_final < 0 else rh_final  # Clamp < 0
-
-            return rh_final
-        else:
-            return -255
-
-
-
 def initsen177(port='/dev/ttyS1'):
 	'''
-	Initialize serial port for SEN0177
+	Initialize serial of particles counter SEN0177 or PMS7003
 	'''
 	try:
 		ser = serial.Serial(port, 9600)
@@ -160,7 +68,7 @@ def checkval(inp):
 
 def readsen177(serial):
 	'''
-	Read data from SEN0177 by serial port and return list of all measured values
+	Read data from SEN0177 or PMS7003 by serial port and return list of all measured values
 	'''
 	try:
 		dane=bytearray(serial.read(32))
@@ -190,21 +98,6 @@ def readsen177(serial):
 	except Exception as e:
 		print ("PM data read error: {}".format(e))
 		return 1
-
-def readtemp():
-	'''
-	Read temperature and humidity from HTU21 on i2c bus 0, return data in list
-	'''
-	try:
-		# Get I2C bus
-		termometr=HTU21D()
-		temp=termometr.read_temperature()
-		humidity=termometr.read_humidity()
-		return [round(temp,2),round(humidity,2)]
-
-	except Exception as e:
-		print ("Temperature data read error: {}".format(e))
-		return 2
 
 def toascii(inp):
 	'''
@@ -244,30 +137,39 @@ def filetowrite():
 
 ser=initsen177()
 inittime=datetime.now().second
+errcnt = 0
+
+if notify.enabled():
+	notify.ready()
+	notify.status("Starting measurements ...")
 
 while True:
 	if inittime!=datetime.now().second:
 		inittime=datetime.now().second
 		print(datetime.now().isoformat())
 		try:
-		    pmy=readsen177(ser)
-		    print(pmy)
-		    tmpy=[-99,-99]
-		    time.sleep(0.7)
+			pmy=readsen177(ser)
+			print(pmy)
+			time.sleep(0.7)
 
-		    if pmy[-1]==1:
-        			fname=filetowrite()
-        			with open(fname, 'a') as f:
-		                    f.write(str(IDstacji)+','+str(datetime.utcnow().year)+','+str(datetime.utcnow().month)+','+str(datetime.utcnow().day)+',' \
-		                    +str(datetime.utcnow().hour)+','+str(datetime.utcnow().minute)+','+str(datetime.utcnow().second)+','+str(date2matlab(datetime.now()))+',' \
-		                    +toascii(pmy)+toascii(tmpy)[:-1]+'\n')
-        			f.closed
-        			blink()
+			if pmy[-1]==1:
+				fname=filetowrite()
+				with open(fname, 'a') as f:
+					f.write(str(IDstacji)+','+str(datetime.utcnow().year)+','+str(datetime.utcnow().month)+','+str(datetime.utcnow().day)+',' \
+					+str(datetime.utcnow().hour)+','+str(datetime.utcnow().minute)+','+str(datetime.utcnow().second)+','+str(date2matlab(datetime.now()))+',' \
+					+toascii(pmy)+'\n')
+				f.closed
+				blink()
+				if notify.enabled():
+					notify.notify()
 
-		    else:
-        			ser.close()
-        			ser=initsen177()
+			else:
+				ser.close()
+				ser=initsen177()
 		except Exception as e:
 				ser.close()
 				ser=initsen177()
 				print("ERROR")
+				errcnt+=1
+				if errcnt > 10:
+					sys.exit(66)
